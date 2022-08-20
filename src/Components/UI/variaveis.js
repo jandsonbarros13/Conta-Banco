d6cec24aa3e99d32bf7097f4f0741b87f91e9ec0
jandsonbarros13 <jandsonbarros87@gmail.com>
2022-08-20 00:22:15 -0300

    Banco

---
+++ src/Components/UI/variaveis.js
@@ -0,0 +1,9 @@
+export const corPrimaria = "red";
+
+export const fundoClaro = "gray";
+export const conteudoClaro = "#f1f1f1";
+export const textoFundoClaro = "black";
+
+export const fundoEscuro = "#363537";
+export const conteudoEscuro = "#5c5b5e";
+export const textoFundoEscuro = "#FAFAFA";
